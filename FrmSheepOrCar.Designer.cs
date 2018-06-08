dd9b3fc88f57771ac9059d895609652ec9385254
hrmhcospro <gengxiaolei@163.com>
2018-06-08 11:07:30 +0800

    Initial commit

---
+++ FrmSheepOrCar.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace CarSheepTest
+{
+    partial class FrmSheepOrCar
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(47, 14);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(202, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 16);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Change door?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "result";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "loop";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 219);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SheepOrCar";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FrmSheepOrCar.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarSheepTest
+{
+    public partial class FrmSheepOrCar : Form
+    {
+        public FrmSheepOrCar()
+        {
+            InitializeComponent();
+        }
+
+        private Random rd = new Random();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int loop = (int)this.numericUpDown1.Value;
+            int getCars = 0;
+            for (int i = 0; i < loop; i++)
+            {
+                getCars += GetCar();
+            }
+
+            this.label1.Text = $"played {loop} times, get {getCars} cars, win percent: {Math.Round((decimal)getCars / loop, 6) * 100}%";
+        }
+
+
+        private int GetCar()
+        {
+            //set sheep and car
+            int[] door = new int[3] { 0, 0, 0 };
+            int carIdx = rd.Next(0, 3);
+            door[carIdx] = 1;
+
+            //choose one door
+            int chooseIdx = rd.Next(0, 3);
+
+            //open a sheep door
+            int fromLeftOrRight = rd.Next(0, 2);
+            int openIdx = 0;
+            if (fromLeftOrRight == 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (i == chooseIdx)
+                        continue;
+
+                    if (door[i] == 0)
+                    {
+                        openIdx = i;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 3 - 1; i >= 0; i--)
+                {
+                    if (i == chooseIdx)
+                        continue;
+
+                    if (door[i] == 0)
+                    {
+                        openIdx = i;
+                        break;
+                    }
+                }
+            }
+
+            //change
+            if (this.checkBox1.Checked)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (i == openIdx)
+                        continue;
+                    if (i == chooseIdx)
+                        continue;
+                    chooseIdx = i;
+                    break;
+                }
+            }
+
+            return door[chooseIdx];
+        }
+    }
+}
